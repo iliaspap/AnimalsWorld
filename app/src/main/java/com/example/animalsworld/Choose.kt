fd86e94c954fec6785d2f6cefe3f924fffe4af6d
John <yiannis.tsouloukas@gmail.com>
2021-03-06 06:50:16 +0200

    oriste

--- app/src/main/java/com/example/animalsworld/Choose.kt
+++ app/src/main/java/com/example/animalsworld/Choose.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.GridLayout
 import android.widget.ImageView
 import android.content.Intent
+import android.view.View
 import android.widget.ImageButton
 
 
@@ -12,11 +13,15 @@ class Choose : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_choose)
-        val Cat0 = findViewById<ImageButton>(R.id.image1)
-        Cat0.setOnClickListener {
+        val lion = findViewById<ImageButton>(R.id.image1)
+        lion.setOnClickListener {
             val intent = Intent(this, animal_activity::class.java).apply{}
             startActivity(intent)
         }
-    }
 
+    }
+    fun back(view: View){
+        val intent = Intent(this,Categories::class.java)
+        startActivity(intent)
+    }
 }
--- app/src/main/java/com/example/animalsworld/animal_activity.kt
+++ app/src/main/java/com/example/animalsworld/animal_activity.kt
@@ -6,22 +6,20 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager.widget.ViewPager
 import androidx.viewpager2.widget.ViewPager2
-//import com.allilepidrasi.Animal.databinding.ActivityMainBinding
 
 class animal_activity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
-        //val binding= ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.animal_activity)
 
         val images = listOf(
             R.drawable.lion,
-            R.drawable.lion
+            R.drawable.lion2
+        //den douleuei me 3
         )
         val adapter = ViewPagerAdapter(images)
         val viewer = findViewById<ViewPager2>(R.id.viewPager)
-        viewer.adapter= adapter
-
+        viewer.adapter = adapter
     }
     fun back(view:View){
         val intent = Intent(this,Choose::class.java)
